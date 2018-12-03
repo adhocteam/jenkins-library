fe4ee691157c96e4a8579b5e06dccdbc24295c94
Robert Fairhead <bob@adhocteam.us>
2018-12-03 15:18:56 -0600

    Error fix and better formatting slack updates

--- vars/slackStatus.groovy
+++ vars/slackStatus.groovy
@@ -1,18 +1,22 @@
-def call(String name, Bool failed=false) {
-    // Remove whitespace and leading # character
-    pr = sh(returnStdout: true, script: 'git log -1 --pretty=%B | grep -o "#[0-9]*"').trim()[1..-1]
-    colorCode = failed ? "danger" : "good"
-    status = failed ? "Failed" : "Success"
+def call(String name, Boolean failed=false) {
+    colorCode = failed ? 'danger' : 'good'
+    status = failed ? 'Failed' : 'Success'
 
     // Strip .git from the end
     githubURL = env.GIT_URL[0..-5]
 
+    // Gets the last commit message and the committer's name
+    commitMsg = sh(returnStdout: true, script: 'git log -1 --pretty="%s <%an>"').trim()
+    // Look for a PR number in the message
+    pr = commitMsg =~ /.*#([0-9]*).*/
+
     if (pr) {
-        msg = "(<${githubURL}|${name}) <${env.RUN_DISPLAY_URL}|Deploy> of <${githubURL}/pulls/${pr}|PR-${pr}> -- ${status}"
+        //Get the matched group with the number
+        prNum = pr[0][1]
+        msg = "(<${githubURL}|${name}) <${env.RUN_DISPLAY_URL}|Deploy> of <${githubURL}/pulls/${prNum}|${commitMsg}> -- ${status}"
     }
     else {
-        short = env.GIT_COMMIT[0..6]
-        msg = "(<${githubURL}|${name}) <${env.RUN_DISPLAY_URL}|Deploy> of <${githubURL}/commit/${env.GIT_COMMIT}|Commit ${short}> -- ${status}"
+        msg = "(<${githubURL}|${name}) <${env.RUN_DISPLAY_URL}|Deploy> of <${githubURL}/commit/${env.GIT_COMMIT}|${commitMsg}> -- ${status}"
     }
 
     slackSend color: colorCode, message: msg
