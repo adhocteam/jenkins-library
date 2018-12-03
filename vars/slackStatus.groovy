879f68c405a84688e1d281c327d0afd3f0731512
Robert Fairhead <bob@adhocteam.us>
2018-12-03 16:46:31 -0600

    Use attachment for formatting

--- vars/slackStatus.groovy
+++ vars/slackStatus.groovy
@@ -1,25 +1,54 @@
-import groovy.json.JsonBuilder
+import groovy.json.JsonOutput
 
 def call(String name, Boolean failed=false) {
-    colorCode = failed ? 'danger' : 'good'
-    status = failed ? 'Failed' : 'Success'
+    colorCode = failed ? '#FF0000' : '#118762'
+    status = failed ? ":no_entry: ${name} Deployment Failed" : ":github-check: ${name} Deployment Success"
 
     // Strip .git from the end
     githubURL = env.GIT_URL[0..-5]
+    repoName = (env.GIT_URL =~ /.*\/([^\/]+).git/)[0][1]
 
     // Gets the last commit message and the committer's name
-    commitMsg = sh(returnStdout: true, script: 'git log -1 --pretty="%s by %an"').trim()
+    shortMsg = sh(returnStdout: true, script: 'git log -1 --pretty="%s"').trim()
+    fullMsg = sh(returnStdout: true, script: 'git log -1 --pretty="%B"').trim()
+    author = sh(returnStdout: true, script: 'git log -1 --pretty="%an"').trim()
     // Look for a PR number in the message
-    pr = commitMsg =~ /.*#([0-9]+).*/
+    pr = shortMsg =~ /.*#([0-9]+).*/
 
     if (pr) {
         //Get the matched group with the number
-        prNum = pr[0][1]
-        msg = "(<${githubURL}|${name}>) <${env.RUN_DISPLAY_URL}|Deploy> of <${githubURL}/pulls/${prNum}|${commitMsg}> -- ${status}"
+        commitURL = "<${githubURL}/pulls/${pr[0][1]}|PR-${pr[0][1]}>"
     }
     else {
-        msg = "(<${githubURL}|${name}>) <${env.RUN_DISPLAY_URL}|Deploy> of <${githubURL}/commit/${env.GIT_COMMIT}|${commitMsg}> -- ${status}"
+        commitURL = "<${githubURL}/commit/${env.GIT_COMMIT}|${env.GIT_COMMIT[0..6]}>"
     }
 
-    slackSend color: colorCode, message: msg
+    attachment = JsonOutput.toJson([
+        attachments: [[
+            fallback: status,
+            color: colorCode,
+            title: status,
+            title_link: env.RUN_DISPLAY_URL,
+            text: fullMsg,
+            fields: [
+                [
+                    title: ':pr:',
+                    value: commitURL,
+                    short: false
+                ],
+                [
+                    title: 'Committer',
+                    value: author,
+                    short: true
+                ]
+                [
+                    title: ':github:',
+                    value: "<${githubURL}|${repoName}>,
+                    short: true
+                ]
+            ],
+        ]]]
+    )
+
+    slackSend color: colorCode, attachments: msg
 }
