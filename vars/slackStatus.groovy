42b5601b4e2c599e0a0c19d54c87dfe3214e64d9
Robert Fairhead <bob@adhocteam.us>
2018-12-04 15:23:49 -0600

    Println debugging?

--- vars/slackStatus.groovy
+++ vars/slackStatus.groovy
@@ -3,7 +3,7 @@ import groovy.json.JsonOutput
 def call(String name, Boolean failed=false) {
     def colorCode = failed ? '#FF0000' : '#118762'
     def attachment = getAttachment(name, failed)
-    echo attachment
+    println attachment
     slackSend color: colorCode, attachments: attachment.trim()
 }
 
