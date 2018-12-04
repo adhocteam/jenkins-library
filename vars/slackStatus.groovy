79abf56cba3864bfd06f7fe594ec58657ee60959
Robert Fairhead <bob@adhocteam.us>
2018-12-04 15:02:12 -0600

    Try using defs to keep variables local

--- vars/slackStatus.groovy
+++ vars/slackStatus.groovy
@@ -1,30 +1,30 @@
 import groovy.json.JsonOutput
 
 def call(String name, Boolean failed=false) {
-    colorCode = failed ? '#FF0000' : '#118762'
-    status = failed ? ":no_entry: ${name} Deployment Failed" : ":github-check: ${name} Deployment Success"
+    def colorCode = failed ? '#FF0000' : '#118762'
+    def status = failed ? ":no_entry: ${name} Deployment Failed" : ":github-check: ${name} Deployment Success"
 
     // Strip .git from the end
-    githubURL = env.GIT_URL[0..-5]
-    repoName = (env.GIT_URL =~ /.*\/([^\/]+).git/)[0][1]
-    githubLink = "<${githubURL}|${repoName}>"
+    def githubURL = env.GIT_URL[0..-5]
+    def repoName = (env.GIT_URL =~ /.*\/([^\/]+).git/)[0][1]
+    def githubLink = "<${githubURL}|${repoName}>"
 
     // Gets the last commit message and the committer's name
-    shortMsg = sh(returnStdout: true, script: 'git log -1 --pretty="%s"').trim()
-    fullMsg = sh(returnStdout: true, script: 'git log -1 --pretty="%B"').trim()
-    author = sh(returnStdout: true, script: 'git log -1 --pretty="%an"').trim()
+    def shortMsg = sh(returnStdout: true, script: 'git log -1 --pretty="%s"').trim()
+    def fullMsg = sh(returnStdout: true, script: 'git log -1 --pretty="%B"').trim()
+    def author = sh(returnStdout: true, script: 'git log -1 --pretty="%an"').trim()
     // Look for a PR number in the message
-    pr = shortMsg =~ /.*#([0-9]+).*/
+    def pr = shortMsg =~ /.*#([0-9]+).*/
 
     if (pr) {
         //Get the matched group with the number
-        commitURL = "<${githubURL}/pulls/${pr[0][1]}|PR-${pr[0][1]}>"
+        def commitURL = "<${githubURL}/pulls/${pr[0][1]}|PR-${pr[0][1]}>"
     }
     else {
-        commitURL = "<${githubURL}/commit/${env.GIT_COMMIT}|${env.GIT_COMMIT[0..6]}>"
+        def commitURL = "<${githubURL}/commit/${env.GIT_COMMIT}|${env.GIT_COMMIT[0..6]}>"
     }
 
-    attachment = JsonOutput.toJson([
+    def attachment = JsonOutput.toJson([
         attachments: [[
             fallback: status,
             color: colorCode,
@@ -51,5 +51,5 @@ def call(String name, Boolean failed=false) {
         ]]]
     )
 
-    slackSend color: colorCode, attachments: msg
+    slackSend color: colorCode, channel: '@bob', attachments: msg
 }
