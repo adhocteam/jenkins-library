e89b3bdbdac9a138dc92e80df3de8f5ed3b23493
Robert Fairhead <bob@adhocteam.us>
2018-12-06 09:44:09 -0600

    Add gometalinter step

---
+++ vars/gometalinter.groovy
@@ -0,0 +1,23 @@
+def call() {
+    sh '''set -eux
+        gometalinter \
+            --disable-all \
+            --enable deadcode \
+            --severity deadcode:error \
+            --enable gofmt \
+            --enable ineffassign \
+            --enable misspell \
+            --enable vet \
+            --tests \
+            --vendor \
+            --deadline 120s \
+            ./...
+
+        gometalinter \
+            --disable-all \
+            --enable golint \
+            --vendor \
+            --skip proto \
+            --deadline 120s \
+            ./...'''
+}
