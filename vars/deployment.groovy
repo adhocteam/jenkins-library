5a31abc46ace8df50e77352b309f4661a24032ae
Robert Fairhead <robertfairhead@users.noreply.github.com>
2019-03-01 22:07:04 -0600

    Add deployment step definition (#3)

---
+++ vars/deployment.groovy
@@ -0,0 +1,56 @@
+def call(String repo, String environment, String url, Closure body) {
+  def deployID = ""
+
+  def createDeployment = """
+    set -eu
+
+    # Create Github Deployment & get the deployment ID
+    curl --request POST \
+      --url https://api.github.com/repos/adhocteam/${repo}/deployments \
+      --header 'accept: application/vnd.github.ant-man-preview+json' \
+      --header "authorization: token \$GH_TOKEN" \
+      --header 'content-type: application/json' \
+      --data '{
+        "ref": "${CHANGE_BRANCH}",
+        "environment": "${environment}",
+        "required_contexts": [],
+        "description": "Starting deployment for ${environment}"
+      }' | jq ".id"
+      """
+
+  def setStatus(String id, String status) {
+    return """
+      set -eu
+
+      curl --request POST \
+        --url "https://api.github.com/repos/adhocteam/${repo}/deployments/${id}/statuses" \
+        --header 'accept: application/vnd.github.ant-man-preview+json, application/vnd.github.flash-preview+json' \
+        --header "authorization: token \$GH_TOKEN" \
+        --header 'content-type: application/json' \
+        --data '{
+        "environment": "${environment}",
+        "state": "${status}",
+        "environment_url": "${url}",
+        "description": "Deployment to ${environment}: ${status}."
+        }'
+        """
+  }
+
+  try {
+    withCredentials([string(credentialsId: 'github-token', variable: 'GH_TOKEN')]) {
+      deployID = sh(returnStdout: true, script: createDeployment).trim()
+    }
+    body.call()
+  } catch (e) {
+    script = setStatus(deployID, 'failure')
+    withCredentials([string(credentialsId: 'github-token', variable: 'GH_TOKEN')]) {
+      sh script
+    }
+    throw e
+  }
+
+  script = setStatus(deployID, 'success')
+  withCredentials([string(credentialsId: 'github-token', variable: 'GH_TOKEN')]) {
+    sh script
+  }
+}
